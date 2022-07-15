ffaf2df7a67af38e5a52b35d1a97555162e0dab5
anonymous <info@1inch.io>
2022-07-15 22:52:53 +0300

    add search for all models

--- web/core/admin.py
+++ web/core/admin.py
@@ -7,6 +7,7 @@ class BotUser(admin.ModelAdmin):
     fields = ["user_id", "first_name", "phone", "city", "username", "name"]
     list_display = ["user_id", "first_name", "phone", "get_city"]
     readonly_fields = ("user_id", "first_name", "username")
+    search_fields = ["user_id"]
 
     def get_city(self, obj):
         return obj.city.title
@@ -18,49 +19,60 @@ class BotUser(admin.ModelAdmin):
 class Mark(admin.ModelAdmin):
     fields = ["title", "is_visible"]
     list_display = ["title", "is_visible"]
-    list_per_page = 15
     search_fields = ["title"]
+    list_per_page = 15
 
 
 @admin.register(Model)
 class Model(admin.ModelAdmin):
     fields = ["mark", "title", "price", "body", "is_visible"]
     list_display = ["mark", "title"]
+    search_fields = ["title"]
+    list_per_page = 15
 
 
 @admin.register(Set)
 class Set(admin.ModelAdmin):
     fields = ["model", "title", "image", "special"]
     list_display = ["model", "title"]
+    search_fields = ["title"]
+    list_per_page = 15
 
 
 @admin.register(Engine)
 class Engine(admin.ModelAdmin):
     fields = ["volume", "power", "type_fuel"]
     list_display = ["volume", "power"]
+    search_fields = ["volume"]
+    list_per_page = 15
 
 
 @admin.register(Transmission)
 class Transmission(admin.ModelAdmin):
     fields = ["title"]
     list_display = ["title"]
+    search_fields = ["title"]
+    list_per_page = 15
 
 
 @admin.register(Wd)
 class Wd(admin.ModelAdmin):
     fields = ["title"]
     list_display = ["title"]
+    search_fields = ["title"]
+    list_per_page = 15
 
 
 @admin.register(City)
 class City(admin.ModelAdmin):
     fields = ["title", "message"]
     list_display = ["title"]
-    list_per_page = 10
     search_fields = ["title"]
+    list_per_page = 10
 
 
 @admin.register(Car)
 class Car(admin.ModelAdmin):
     fields = ["title", "set_id", "image", "price", "engine_id", "transmission_id", "wd_id", "expenditure", "city_id", "mark_id"]
     list_display = ["title", "price"]
+    list_per_page = 20

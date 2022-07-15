61245d8796cf23ebb9de3b149095cc53e6e1b3bb
anonymous <info@1inch.io>
2022-07-15 22:28:13 +0300

    disable start function

--- bot/commands/meeting.py
+++ bot/commands/meeting.py
@@ -7,7 +7,7 @@ from objects.globals import dp, bot
 from objects import globals
 from states.states import Meeting
 from utils.api.requests import Requests
-from .start import start
+# from .start import start
 
 
 api_requests = Requests()
@@ -29,7 +29,8 @@ async def get_name(message: Message, state: FSMContext):
 @dp.message_handler(state=Meeting.city)
 async def get_city(message: Message, state: FSMContext):
     if message.text == "/start":
-        return await start(message, state)
+        #return await start(message, state)
+        pass
 
     city = re.sub("[^а-яА-Я]", "", message.text)
     welcome_page = globals.root.find("welcome")
@@ -43,4 +44,5 @@ async def get_city(message: Message, state: FSMContext):
             return await message.answer(welcome_page.find("city_is_not_found").text)
         else:
             await message.answer(response.get("message"))
-            return await start(message, state)
+            #return await start(message, state)
+            pass

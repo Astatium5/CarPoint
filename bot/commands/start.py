ee829b4b9b82fef2288d3dab2918fa33e18b3eda
anonymous <info@1inch.io>
2022-07-19 15:04:33 +0300

    delete inlines

--- bot/commands/start.py
+++ bot/commands/start.py
@@ -1,8 +1,6 @@
 import hashlib
-from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 
-from aiogram.types import InlineQuery, \
-    InputTextMessageContent, InlineQueryResultArticle
 from aiogram.dispatcher.storage import FSMContext
 from requests import request
 
@@ -46,22 +44,9 @@ async def start(message: Message, state: FSMContext):
         await message.answer(welcome_page.find("is_empty").text.format(bot_info.first_name))
         return await Meeting.name.set()
 
-    """
-    await state.finish()
-
-    markup = InlineKeyboardMarkup(
-        inline_keyboard=[
-            [InlineKeyboardButton(text="Search", switch_inline_query_current_chat="")]
-        ]
-    )
-
-    return await msg.answer(
-        text=F"Hello, I'm bot!", reply_markup=markup)
-    """
-
+"""
 @dp.inline_handler()
 async def inline_echo(query: InlineQuery):
-    """
     items = []
     text = query.query or 'echo'
     input_content = InputTextMessageContent(text)
@@ -73,8 +58,8 @@ async def inline_echo(query: InlineQuery):
     )
     items.append(item)
     await globals.bot.answer_inline_query(query.id, results=items, cache_time=1)
-    """
 
     r = InlineQueryResultArticle(id='1', title='SONY VTC6 3000 mah 30A 18650',  input_message_content=InputTextMessageContent('Аккумулятор 1'), description='blah', hide_url=True, thumb_url='https://via.placeholder.com/50')
     r2 = InlineQueryResultArticle(id='2', title='SONY VTC5A 2600 mah 35A 18650',  input_message_content=InputTextMessageContent('Аккумулятор 2'), description='blah', hide_url=True, thumb_url='https://via.placeholder.com/50')
     await globals.bot.answer_inline_query(query.id, [r, r2])
+"""

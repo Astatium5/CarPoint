12b6a4a1c72bab3ccd3417c8e98cc9af8a77144a
anonymous <info@1inch.io>
2022-07-19 15:07:27 +0300

    add convert for another types

---
+++ web/core/converts.py
@@ -0,0 +1,17 @@
+class FloatUrlParameterConverter:
+    regex = '[0-9]+\.?[0-9]+'
+
+    def to_python(self, value):
+        return float(value)
+
+    def to_url(self, value):
+        return str(value)
+
+
+class BooleanUrlParameterConverter:
+    regex = "$"
+    def to_python(self, value):
+        return bool(value)
+
+    def to_url(self, value):
+        return str(value)

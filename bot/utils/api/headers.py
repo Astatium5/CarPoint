0aecc5cdba17c5ff1977082decfb996f56bd2026
anonymous <info@1inch.io>
2022-07-19 15:06:17 +0300

    convert to header, retype vars

--- bot/utils/api/headers.py
+++ bot/utils/api/headers.py
@@ -1,2 +1,11 @@
 HEADERS = {'Content-Type': 'application/json',
-           'Accept': 'application/json'}
+           'Accept': 'application/json',
+           'Accept-Encoding': 'gzip, deflate, br',
+           'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7'}
+
+
+def convert_to_headers(**kwargs):
+    for k, v in kwargs.items():
+        if isinstance(v, bool) or isinstance(v, int) or isinstance(v, float):
+            kwargs[k] = str(v)
+    return kwargs

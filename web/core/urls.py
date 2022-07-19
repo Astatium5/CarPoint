c1abef867b5f720bec2ad720358d2bf7e3c6dbf0
anonymous <info@1inch.io>
2022-07-19 15:07:43 +0300

    add find car url, set for another url new param

--- web/core/urls.py
+++ web/core/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path, include
+from django.urls import path, register_converter
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 from .views import *
+from .converts import *
+
+register_converter(FloatUrlParameterConverter, "float")
+register_converter(BooleanUrlParameterConverter, "bool")
 
 urlpatterns = [
     # ===WEB urls==
@@ -17,6 +21,7 @@ urlpatterns = [
     path("api/check_phone/<int:user_id>", API.CheckPhoneView.as_view(), name="check_phone"),
     path("api/set_phone/<int:user_id>/<int:phone>", API.SetPhoneView.as_view(), name="set_phone"),
     path("api/get_all_marks", API.GetAllMarksView.as_view(), name="get_all_marks"),
-    path("api/get_all_bodies", API.GetAllBodiesView.as_view(), name="get_all_bodies"),
-    path("api/get_all_fuel_types", API.GetAllFuelTypesView.as_view(), name="get_all_fuel_types"),
+    path("api/get_all_bodies/<str:mark>", API.GetAllBodiesView.as_view(), name="get_all_bodies"),
+    path("api/get_all_fuel_types/<str:mark>", API.GetAllFuelTypesView.as_view(), name="get_all_fuel_types"),
+    path("api/find_car/<str:body>/<str:fuel_type>/<str:transmission>", API.FindCarView.as_view(), name="find_car")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

fb77598f5bd69a6753ddfaa053fd494161e60d77
anonymous <info@1inch.io>
2022-07-19 15:06:00 +0300

    check for headers

--- bot/utils/api/execute.py
+++ bot/utils/api/execute.py
@@ -2,17 +2,22 @@ import requests
 
 from log.logger import logger
 from config.config import Config
-from .headers import HEADERS
+from .headers import HEADERS, convert_to_headers
 from .serializer import serialize_content
 
 
 config = Config()
 
-def make_request(path: str, **kwargs):
-    values = [str(v) for v in kwargs.values()]
-    args_string = "/".join(values)
-    url = (rF"http://{config.host}:{config.port}/{path}{args_string}")
-    response = requests.get(url, headers=HEADERS, timeout=0.5)
+def make_request(path: str, headers=None, **kwargs):
+    if headers:
+        HEADERS.update(convert_to_headers(**headers))
+    if kwargs:
+        values = [str(v) for v in kwargs.values()]
+        args_string = "/".join(values)
+        url = (rF"http://{config.host}:{config.port}/{path}{args_string}")
+    else:
+        url = (rF"http://{config.host}:{config.port}/{path}")
+    response = requests.get(url, headers=HEADERS, timeout=5)
     if response.status_code == 200:
         return serialize_content(response.content)
     else:

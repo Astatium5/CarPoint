9fe4305ec0e91fe4164818f945c523d6357f62a3
anonymous <info@1inch.io>
2022-07-21 22:39:16 +0300

    delete comments, reset OfferMetaData

--- bot/commands/start.py
+++ bot/commands/start.py
@@ -18,6 +18,7 @@ api_requests = Requests()
 @dp.message_handler(commands="start")
 async def start(message: Message, state: FSMContext):
     globals.start = start
+    globals.offer_metadata = OfferMetaData() # Init OfferMetaData and set to vatiable
     await state.finish()
     response = api_requests.create_user(user_id=message.from_user.id, first_name=message.from_user.first_name,
         username=message.from_user.username) # Create new user
@@ -43,23 +44,3 @@ async def start(message: Message, state: FSMContext):
     else:
         await message.answer(welcome_page.find("is_empty").text.format(bot_info.first_name))
         return await Meeting.name.set()
-
-"""
-@dp.inline_handler()
-async def inline_echo(query: InlineQuery):
-    items = []
-    text = query.query or 'echo'
-    input_content = InputTextMessageContent(text)
-    result_id: str = hashlib.md5(text.encode()).hexdigest()
-    item = InlineQueryResultArticle(
-        id=result_id,
-        title=f'Result {text!r}',
-        input_message_content=input_content,
-    )
-    items.append(item)
-    await globals.bot.answer_inline_query(query.id, results=items, cache_time=1)
-
-    r = InlineQueryResultArticle(id='1', title='SONY VTC6 3000 mah 30A 18650',  input_message_content=InputTextMessageContent('Аккумулятор 1'), description='blah', hide_url=True, thumb_url='https://via.placeholder.com/50')
-    r2 = InlineQueryResultArticle(id='2', title='SONY VTC5A 2600 mah 35A 18650',  input_message_content=InputTextMessageContent('Аккумулятор 2'), description='blah', hide_url=True, thumb_url='https://via.placeholder.com/50')
-    await globals.bot.answer_inline_query(query.id, [r, r2])
-"""

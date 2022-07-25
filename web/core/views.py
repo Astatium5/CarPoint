816d217bb9d5599b8649d3effcc69e8fb9a6acb3
anonymous <info@1inch.io>
2022-07-25 11:49:33 +0300

    change from and to for min and max in price rng

--- web/core/views.py
+++ web/core/views.py
@@ -14,23 +14,23 @@ from .models import *
 
 PRICE_RANGE = {
     "87506fd2b91be8b7ab7b59d069c42d40": {
-        "from": 500000,
-        "to": 2000000
+        "min": 500000,
+        "max": 2000000
     },
 
     "1ee1876784dfba4421dfbc93272053a8": {
-        "from": 2000000,
-        "to": 4000000
+        "min": 2000000,
+        "max": 4000000
     },
 
     "af499ea026c3e952d324d7af4cf7aaee": {
-        "from": 4000000,
-        "to": 6000000
+        "min": 4000000,
+        "max": 6000000
     },
 
     "2a3225e2decd960cebe8c4de135f59a0": {
-        "from": 6000000,
-        "to": None
+        "min": 6000000,
+        "max": None
     },
 } # Keys is ids price range.
 
@@ -138,16 +138,17 @@ class API:
                 user_id = int(headers.get("Userid"))
                 if bool(headers.get("Isrange")):
                     price_range = PRICE_RANGE.get(headers.get("Rangeid"))
-                    _from = price_range.get("from")
-                    _to = price_range.get("to")
+                    min_p = price_range.get("min")
+                    max_p = price_range.get("max")
                     if bool(headers.get("Isvolume")):
                         try:
                             user = BotUser.objects.get(user_id=user_id)
                             mark = Mark.objects.filter(title=headers.get("Mark")).get()
                             transmission_obj = Transmission.objects.filter(title=transmission).get()
-                            cars = Car.objects.filter(mark=mark, price__range=[_from, _to], city=user.city, transmission=transmission_obj).all()
+                            cars = Car.objects.filter(mark=mark, price__range=[min_p, max_p], city=user.city, transmission=transmission_obj).all()
                             if cars:
                                 cars = [car.to_dict() for car in cars]
+                                print(len(cars))
                                 return HttpResponse(json.dumps({"response": True, "cars": cars}), content_type='application/json')
                         except Exception as e:
                             logger.error(e)

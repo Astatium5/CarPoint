e612011098bda5fd2f08efb4aca88bc8ee756ea3
anonymous <info@1inch.io>
2022-07-29 12:42:24 +0300

    check for group

--- bot/commands/start.py
+++ bot/commands/start.py
@@ -17,31 +17,32 @@ api_requests = Requests()
 
 @dp.message_handler(commands="start")
 async def start(message: Message, state: FSMContext):
-    globals.start = start
-    globals.offer_metadata = OfferMetaData() # Init OfferMetaData and set to vatiable
-    await state.finish()
-    response = api_requests.create_user(user_id=message.from_user.id, first_name=message.from_user.first_name,
-        username=message.from_user.username) # Create new user
+    if message.chat.type != "group":
+        globals.start = start
+        globals.offer_metadata = OfferMetaData() # Init OfferMetaData and set to vatiable
+        await state.finish()
+        response = api_requests.create_user(user_id=message.from_user.id, first_name=message.from_user.first_name,
+            username=message.from_user.username) # Create new user
 
-    bot_info = await bot.get_me() # Get bot info
-    welcome_page = globals.root.find("welcome")
+        bot_info = await bot.get_me() # Get bot info
+        welcome_page = globals.root.find("welcome")
 
-    if not response.get("response"):
-        if not response.get("name"):
+        if not response.get("response"):
+            if not response.get("name"):
+                await message.answer(welcome_page.find("is_empty").text.format(bot_info.first_name))
+                return await Meeting.name.set()
+            elif not response.get("city"):
+                await message.answer(welcome_page.find("is_not_city").text)
+                return await Meeting.city.set()
+            else:
+                reply_markup = ReplyKeyboardMarkup(resize_keyboard=True,
+                    keyboard=[
+                        [KeyboardButton(text="Получить предложение"), KeyboardButton(text="Для инвесторов")],
+                        [KeyboardButton(text="Контакты"), KeyboardButton(text="О проекте"), KeyboardButton(text="Для дистрибьюторов")],
+                        [KeyboardButton(text="Как мы работаем")]
+                    ])
+                return await message.answer(welcome_page.find("is_not_empty").text.format(bot_info.first_name),
+                    reply_markup=reply_markup)
+        else:
             await message.answer(welcome_page.find("is_empty").text.format(bot_info.first_name))
             return await Meeting.name.set()
-        elif not response.get("city"):
-            await message.answer(welcome_page.find("is_not_city").text)
-            return await Meeting.city.set()
-        else:
-            reply_markup = ReplyKeyboardMarkup(resize_keyboard=True,
-                keyboard=[
-                    [KeyboardButton(text="Получить предложение"), KeyboardButton(text="Для инвесторов")],
-                    [KeyboardButton(text="Контакты"), KeyboardButton(text="О проекте"), KeyboardButton(text="Для дистрибьюторов")],
-                    [KeyboardButton(text="Как мы работаем")]
-                ])
-            return await message.answer(welcome_page.find("is_not_empty").text.format(bot_info.first_name),
-                reply_markup=reply_markup)
-    else:
-        await message.answer(welcome_page.find("is_empty").text.format(bot_info.first_name))
-        return await Meeting.name.set()

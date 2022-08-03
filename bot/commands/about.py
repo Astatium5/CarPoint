ab2dd946d767bca4af272a0319e8f3ce7b334c3e
anonymous <info@1inch.io>
2022-08-03 17:14:56 +0300

    add state handler

--- bot/commands/about.py
+++ bot/commands/about.py
@@ -1,10 +1,12 @@
 from aiogram.types import Message
+from aiogram.dispatcher.storage import FSMContext
 
 from objects.globals import dp
 from objects import globals
 
 
-@dp.message_handler(lambda message: message.text == "О проекте")
-async def about(message: Message):
+@dp.message_handler(lambda message: message.text == "О проекте", state="*")
+async def about(message: Message, state: FSMContext):
+    await state.finish()
     about_page = globals.root.find("about")
     return await message.answer(about_page.text)

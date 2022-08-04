c908a9c7c348774f86ca2e152e81510059a38ce4
anonymous <info@1inch.io>
2022-08-04 18:13:36 +0300

    import contact_me_markup from keyboard

--- bot/commands/for_investors.py
+++ bot/commands/for_investors.py
@@ -5,10 +5,11 @@ from aiogram.dispatcher.storage import FSMContext
 
 from objects.globals import dp
 from objects import globals
+from keyboard.keyboard import *
 
 
 @dp.message_handler(lambda message: message.text == "Для инвесторов", state="*")
 async def for_investors(message: Message, state: FSMContext) -> Message:
     await state.finish()
     page: Any = globals.root.find("for_investors")
-    return await message.answer(page.text)
+    return await message.answer(page.text, reply_markup=contact_me_markup)

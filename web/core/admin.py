0d4e9ca41cc709a97a69927fcd902ee99a1c7806
anonymous <info@1inch.io>
2022-08-04 19:13:18 +0300

    delete types

--- web/core/admin.py
+++ web/core/admin.py
@@ -8,10 +8,10 @@ from .models import *
 
 @admin.register(BotUser)
 class BotUser(admin.ModelAdmin):
-    fields: list[str] = ["user_id", "first_name", "phone", "city", "username", "name"]
-    list_display: list[str] = ["user_id", "first_name", "phone", "get_city"]
-    readonly_fields: tuple[Literal["user_id"], Literal["first_name"], Literal["username"]] = ("user_id", "first_name", "username",)
-    search_fields: list[str] = ["user_id"]
+    fields = ["user_id", "first_name", "phone", "city", "username", "name"]
+    list_display = ["user_id", "first_name", "phone", "get_city"]
+    readonly_fields = ("user_id", "first_name", "username",)
+    search_fields = ["user_id"]
 
     def get_city(self, obj) -> Any:
         return obj.city.title
@@ -21,65 +21,65 @@ class BotUser(admin.ModelAdmin):
 
 @admin.register(Mark)
 class Mark(admin.ModelAdmin):
-    fields: list[str] = ["title", "is_visible"]
-    list_display: list[str] = ["title", "is_visible"]
-    search_fields: list[str] = ["title"]
+    fields = ["title", "is_visible"]
+    list_display = ["title", "is_visible"]
+    search_fields = ["title"]
     list_per_page: int = 15
 
 
 @admin.register(Model)
 class Model(admin.ModelAdmin):
-    fields: list[str] = ["mark", "title", "price", "body", "is_visible"]
-    list_display: list[str] = ["mark", "title"]
-    search_fields: list[str] = ["title"]
+    fields = ["mark", "title", "price", "body", "is_visible"]
+    list_display = ["mark", "title"]
+    search_fields = ["title"]
     list_per_page: int = 15
 
 
 @admin.register(Set)
 class Set(admin.ModelAdmin):
-    fields: list[str] = ["model", "title", "image", "special"]
-    list_display: list[str] = ["model", "title"]
-    search_fields: list[str] = ["title"]
+    fields = ["model", "title", "image", "special"]
+    list_display = ["model", "title"]
+    search_fields = ["title"]
     list_per_page: int = 15
 
 
 @admin.register(Engine)
 class Engine(admin.ModelAdmin):
-    fields: list[str] = ["volume", "power", "type_fuel"]
-    list_display: list[str] = ["volume", "power", "type_fuel"]
-    search_fields: list[str] = ["volume"]
+    fields = ["volume", "power", "type_fuel"]
+    list_display = ["volume", "power", "type_fuel"]
+    search_fields = ["volume"]
     list_per_page: int = 15
 
 
 @admin.register(Transmission)
 class Transmission(admin.ModelAdmin):
-    fields: list[str] = ["title"]
-    list_display: list[str] = ["title"]
-    search_fields: list[str] = ["title"]
+    fields = ["title"]
+    list_display = ["title"]
+    search_fields = ["title"]
     list_per_page: int = 15
 
 
 @admin.register(Wd)
 class Wd(admin.ModelAdmin):
-    fields: list[str] = ["title"]
-    list_display: list[str] = ["title"]
-    search_fields: list[str] = ["title"]
+    fields = ["title"]
+    list_display = ["title"]
+    search_fields = ["title"]
     list_per_page: int = 15
 
 
 @admin.register(City)
 class City(admin.ModelAdmin):
-    fields: list[str] = ["title", "message"]
-    list_display: list[str] = ["title"]
-    search_fields: list[str] = ["title"]
+    fields = ["title", "message"]
+    list_display = ["title"]
+    search_fields = ["title"]
     list_per_page: int = 10
 
 
 @admin.register(Car)
 class Car(admin.ModelAdmin):
-    fields: list[str] = ["title", "set", "image", "price", "engine", "transmission", "wd", "expenditure", "city", "mark"]
+    fields = ["title", "set", "image", "price", "engine", "transmission", "wd", "expenditure", "city", "mark"]
     list_display = ["title", "price", "get_image"]
-    list_filter: tuple[Literal['mark']] = ("mark",)
+    list_filter = ("mark",)
     search_fields = ["title"]
     list_per_page: int = 20
 

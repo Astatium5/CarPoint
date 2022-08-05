f958fdd76c36a51c7bd8764497b5928cdced8ef5
anonymous <info@1inch.io>
2022-08-05 11:17:46 +0300

    reanem convert_to_headers return type

--- bot/utils/api/headers.py
+++ bot/utils/api/headers.py
@@ -6,7 +6,7 @@ HEADERS: dict = {'Content-Type': 'application/json',
            'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7'}
 
 
-def convert_to_headers(**kwargs) -> dict[str, Any]:
+def convert_to_headers(**kwargs) -> dict:
     for k, v in kwargs.items():
         if isinstance(v, bool) or isinstance(v, int) or isinstance(v, float):
             kwargs[k] = str(v)

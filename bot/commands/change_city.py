330bebaf1fcf8f51f58abed133e9066e540a1b76
anonymous <info@1inch.io>
2022-08-08 13:33:19 +0300

    add change city

---
+++ bot/commands/change_city.py
@@ -0,0 +1,12 @@
+from aiogram.types import Message
+
+from objects.globals import dp
+from objects import globals
+from states.states import Meeting
+
+
+@dp.message_handler(lambda message: message.text == "Сменить город")
+async def change_city(message: Message):
+    welcome_page = globals.root.find("welcome")
+    await message.answer(welcome_page.find("is_not_city").text)
+    return await Meeting.city.set()

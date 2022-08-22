3dfb166ccc9bbe3ec723a623ef1160430a3a42eb
anonymous <info@1inch.io>
2022-08-23 01:05:30 +0300

    refactoring get ip

--- web/core/views.py
+++ web/core/views.py
@@ -477,5 +477,9 @@ def leaveRequest(car_id, name, city, tel, email, address):
     return response
 
 def get_client_ip(request):
-    ip = request.META.get('REMOTE_ADDR')
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
     return ip

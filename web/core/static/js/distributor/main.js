3f95ff6862fca9a17cde97c31e81bed1b1fe1280
amtp1 <magomedov.abdul20012@gmail.com>
2022-10-10 21:39:26 +0300

    refactoring, SaveCompanyData

--- web/core/static/js/distributor/main.js
+++ web/core/static/js/distributor/main.js
@@ -1,14 +1,26 @@
 $(document).ready(function(){
-    $('form input').change(function () {
-      $('form p').text(this.files.length + " файл(-а) выбрано");
-    });
-});
+    $('#upload_file').change(function () {
+      var preview = document.getElementById('company-image');
+      var file = document.getElementById("upload_file").files[0];
+      var reader = new FileReader();
 
+      reader.onloadend = function () {
+        preview.src = reader.result;
+      }
+
+      if (file) {
+        reader.readAsDataURL(file);
+      } else {
+        preview.src = "";
+      }
+      preview.setAttribute("exp", "new")
+    })
+})
 
 function uploadFile(){
-  let file = document.getElementById("upload-input").files[0];
+  let file = document.getElementById("docs").files[0];
   if (!file){
-    alert("Нужно загрузить файл!");
+    alert("Нужно выбрать файл для загрузки!");
   }else{
     let modal = $(".modal")
     modal.modal("show");
@@ -27,17 +39,21 @@ function uploadFile(){
     }).then(function (response) {
       response.json().then(
           function (data) {
+              let option  = {
+                animation: true,
+                delay: 5000
+              }
               refreshFileInput();
               if (data["response"]) {
-                let option  = {
-                  animation: true,
-                  delay: 5000
-                }
                 const myToast = document.getElementById('doneToast');
                 let bsToast = new bootstrap.Toast(myToast, option);
                 bsToast.show();
               }else{
-                
+                if (data["type"] == "NotDistribAccount"){
+                  const myToast = document.getElementById('failToast');
+                  let bsToast = new bootstrap.Toast(myToast, option);
+                  bsToast.show();
+                }
               }
           }
       )
@@ -46,7 +62,7 @@ function uploadFile(){
 }
 
 function logout(){
-  fetch("logout", {
+  fetch("/logout", {
     method: "POST",
     contentType: 'application/json',
     headers: {
@@ -57,7 +73,7 @@ function logout(){
     response.json().then(
         function (data) {
             if (data["response"]) {
-                window.location.replace("distributor");
+                window.location.replace("/distributor");
             }
         }
     )
@@ -69,8 +85,48 @@ function closeToast(id){
 }
 
 function refreshFileInput(){
-  let modal = $(".modal")
+  let modal = $(".modal");
   modal.modal("hide");
-  $('form p').text("Перетащите файл или нажмите на область");
-  $( "#upload-input" ).val("");
+  $( "#docs" ).val("");
+}
+
+
+function saveCompanyData(){
+  let image = document.getElementById("company-image");
+  let title = document.getElementById("title-input");
+  let exp = image.getAttribute("exp");
+  if (exp!="new"){
+    return alert("Нужно загрузить логотип компании!");
+  }else if (!title.value){
+    return alert("Нужно ввести название компании!");
+  }else{
+    let data = new FormData();
+    let file = document.getElementById("upload_file").files[0];
+    data.append("image", file);
+    data.append("title", title.value);
+
+    fetch("save_data", {
+      method: "POST",
+      body: data,
+      contentType: 'application/json',
+      headers: {
+          "X-Requested-With": "XMLHttpRequest",
+          "X-CSRFToken": $.cookie("csrftoken")
+      },
+    }).then(function (response) {
+      response.json().then(
+          function (data) {
+              if (data["response"]) {
+                let option  = {
+                  animation: true,
+                  delay: 5000
+                }
+                const myToast = document.getElementById('doneSaveCompanyDataToast');
+                let bsToast = new bootstrap.Toast(myToast, option);
+                bsToast.show();
+              }
+          }
+      )
+    })
+  }
 }

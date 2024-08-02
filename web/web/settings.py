d1b7c9aa3c51d232a53302ed0a8753c907188677
Dmitrii Golubenko <dmitro00@bk.ru>
2024-08-02 04:02:33 -0500

    Made the project functional locally

--- web/web/settings.py
+++ web/web/settings.py
@@ -100,6 +100,10 @@ DATABASES = {
         'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
+        'OPTIONS': {
+            'sql_mode': 'traditional',
+        }
     }
 }
 
